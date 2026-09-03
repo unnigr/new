95e16cf75237c95289f3eb1b54f6e89691117e0f
unnigr <109684132+unnigr@users.noreply.github.com>
2026-09-03 23:33:19 +0530

    Upgrade Android and Kotlin plugin versions
    Updated plugin versions for Android and Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,5 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
-}
-android {
-    namespace = "com.unni.simplemusicplayer"
-    compileSdk = 35
-    defaultConfig {
-        applicationId = "com.unni.simplemusicplayer"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-}
-dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.3.2")
-    implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.media3:media3-exoplayer:1.10.1")
-    implementation("androidx.media3:media3-session:1.10.1")
-    implementation("androidx.media3:media3-ui:1.10.1")
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
